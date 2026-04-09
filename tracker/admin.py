9117d9ef03b1aeedea5b9c476e2fb2181e97abe0
TrueBloodDK <dkaras2016@gmail.com>
2026-04-09 10:09:46 +0500

    model is added for admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PressureRecord
 
-# Register your models here.
+
+@admin.register(PressureRecord)
+class PressureRecordAdmin(admin.ModelAdmin):
+    list_display = ['user', 'systolic', 'diastolic', 'pulse', 'created_at']
+    list_filter = ['user', 'created_at']
+    search_fields = ['user__username']
+    ordering = ['-created_at']
